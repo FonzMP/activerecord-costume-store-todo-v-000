d127ef4b278f3660fdd18588660efb2ac4f9d626
Alex Wilson <fonzmp@gmail.com>
2018-06-05 01:28:01 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -35,8 +35,7 @@ ActiveRecord::Schema.define(version: 3) do
 
   create_table "haunted_houses", force: :cascade do |t|
     t.string "name"
-    t.integer "price"
-    t.string "image_url"
+    t.string "location"
     t.string "size"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
