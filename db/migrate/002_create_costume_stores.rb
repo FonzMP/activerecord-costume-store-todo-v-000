c3b7a69f729b57d042ec14343323c69b74f667ed
Alex Wilson <fonzmp@gmail.com>
2018-06-05 01:22:57 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -7,7 +7,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       t.string :name
       t.string :location
       t.integer :costume_inventory
-      t.string :image_url
+      t.string :number_of_employees
       t.string :size
       t.timestamps
     end
