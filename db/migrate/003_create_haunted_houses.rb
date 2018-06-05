39e51016cb3bb589507afa4c177e60aa39eea42e
Alex Wilson <fonzmp@gmail.com>
2018-06-05 01:29:13 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -9,6 +9,8 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
       t.string :theme
       t.integer :price
       t.boolean :family_friendly
+      t.datetime :opening_time
+      t.datetime :closing_time
     end
   end
 
